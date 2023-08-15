e200d54acfc29e8302ec669b6ee6d0269d19f806
KeyStore <keystore-api@users.noreply.github.com>
2023-08-15 22:38:07 +0300

    Fix: Modified default Guzzle header to avoid conflicts with certain firewalls

--- src/clients/HttpGuzzleClient.php
+++ src/clients/HttpGuzzleClient.php
@@ -32,6 +32,9 @@ class HttpGuzzleClient extends Client implements HttpClientInterface
         $config = array_merge($config, [
             'base_uri' => self::$baseUrl,
             'verify' => false,
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            ],
         ]);
         parent::__construct($config);
     }
